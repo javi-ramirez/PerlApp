c7ddd98a033153243eca8810237f0f99439ceddf
Javier-Ramirez <javi.guti.rama@gmail.com>
2020-04-01 14:33:55 -0600

    Añadir en Main comentario de que ya me funciona el proyecto

--- app/src/main/java/com/example/perlapp/MainActivity.java
+++ app/src/main/java/com/example/perlapp/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    //Comentario de que Javier ya tiene el proyecto funcionando
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
