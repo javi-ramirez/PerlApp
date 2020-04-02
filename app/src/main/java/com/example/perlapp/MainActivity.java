f80bc4976ff8ea3483d57e0eceff360b90ec1d04
Keuri Machain <keuri_machain16@outlook.es>
2020-04-01 20:45:14 -0600

    Creo ya se añadio la primer pantalla

--- app/src/main/java/com/example/perlapp/MainActivity.java
+++ app/src/main/java/com/example/perlapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.perlapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
     //Comentario de que Javier ya tiene el proyecto funcionando bien
@@ -9,9 +14,18 @@ public class MainActivity extends AppCompatActivity {
     //okey no jalaa
     //sigue sin jalar
     //intento 123
+
+
+    EditText edtNombreUsuario;
+    EditText edtComentario;
+    Button btnEnviar;
+    ListView listMostrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
