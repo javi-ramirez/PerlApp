5bb00298e15de21b368d62b1770a2708faaa9d1c
Keuri Machain <keuri_machain16@outlook.es>
2020-04-01 20:21:52 -0600

    okey no jala okey no jala x2

--- app/src/main/java/com/example/perlapp/MainActivity.java
+++ app/src/main/java/com/example/perlapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
     //Comentario de que Javier ya tiene el proyecto funcionando bien
     ///hola intento 1111111
+    //okey
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
